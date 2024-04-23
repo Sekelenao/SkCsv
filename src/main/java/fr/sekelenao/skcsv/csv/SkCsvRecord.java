497ff40a8dfdd073579cd3b2bf6338ad89e78c66
Sekelenao <Sekelenao@protonmail.com>
2024-04-23 19:19:40 +0200

    Moved static method for invoking accessor into the interface

---
+++ src/main/java/fr/sekelenao/skcsv/csv/SkCsvRecord.java
@@ -0,0 +1,39 @@
+package fr.sekelenao.skcsv.csv;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public interface SkCsvRecord {
+
+    private static Object secureInvoke(Method accessor, Object instance) {
+        Objects.requireNonNull(accessor);
+        Objects.requireNonNull(instance);
+        try{
+            return accessor.invoke(instance);
+        }catch (IllegalAccessException e){
+            throw new IllegalAccessError();
+        } catch(InvocationTargetException exception) {
+            switch (exception.getCause()){
+                case RuntimeException e -> throw e;
+                case Error e -> throw e;
+                default -> throw new AssertionError();
+            }
+        }
+    }
+
+    default Iterable<String> recordValues() {
+        var components = this.getClass().getRecordComponents();
+        var values = new ArrayList<String>(components.length);
+        for(var component: components){
+            if(component.isAnnotationPresent(CsvColumn.class)){
+                var obj = secureInvoke(component.getAccessor(), this);
+                var value = obj == null ? "" : String.valueOf(obj);
+                values.add(value);
+            }
+        }
+        return values;
+    }
+
+}
