27551e5db7376f0debadb1931ca169c91b63238e
Sekelenao <Sekelenao@protonmail.com>
2024-04-23 20:51:52 +0200

    Moved annotation inside interface

--- src/main/java/fr/sekelenao/skcsv/csv/SkCsvRecord.java
+++ src/main/java/fr/sekelenao/skcsv/csv/SkCsvRecord.java
@@ -1,5 +1,9 @@
 package fr.sekelenao.skcsv.csv;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -7,15 +11,21 @@ import java.util.Objects;
 
 public interface SkCsvRecord {
 
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.RECORD_COMPONENT})
+    @interface CsvColumn {
+        String value() default "";
+    }
+
     private static Object secureInvoke(Method accessor, Object instance) {
         Objects.requireNonNull(accessor);
         Objects.requireNonNull(instance);
-        try{
+        try {
             return accessor.invoke(instance);
-        }catch (IllegalAccessException e){
+        } catch (IllegalAccessException e) {
             throw new IllegalAccessError();
-        } catch(InvocationTargetException exception) {
-            switch (exception.getCause()){
+        } catch (InvocationTargetException exception) {
+            switch (exception.getCause()) {
                 case RuntimeException e -> throw e;
                 case Error e -> throw e;
                 default -> throw new AssertionError();
@@ -23,29 +33,11 @@ public interface SkCsvRecord {
         }
     }
 
-    private static <T extends Record & SkCsvRecord> Iterable<String> generateHeaders(Class<T> type) {
-        Objects.requireNonNull(type);
-        var headers = new ArrayList<String>();
-        var fields =  type.getDeclaredFields();
-        for(var field: fields){
-            if(field.isAnnotationPresent(CsvColumn.class)){
-                var annotation = field.getAnnotation(CsvColumn.class);
-                var header = annotation.value();
-                if(header.isEmpty()){
-                    headers.add(field.getName());
-                } else {
-                    headers.add(header);
-                }
-            }
-        }
-        return headers;
-    }
-
-    default Iterable<String> recordValues() {
+    default Iterable<String> skRecordValues() {
         var components = this.getClass().getRecordComponents();
         var values = new ArrayList<String>(components.length);
-        for(var component: components){
-            if(component.isAnnotationPresent(CsvColumn.class)){
+        for (var component : components) {
+            if (component.isAnnotationPresent(SkCsvRecord.CsvColumn.class)) {
                 var obj = secureInvoke(component.getAccessor(), this);
                 var value = obj == null ? "" : String.valueOf(obj);
                 values.add(value);
@@ -53,7 +45,5 @@ public interface SkCsvRecord {
         }
         return values;
     }
-
-
 
 }
