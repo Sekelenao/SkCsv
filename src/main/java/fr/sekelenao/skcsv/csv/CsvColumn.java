bd9a7c77587ed46b8aaedf24cb3661a7fffcd503
Sekelenao <Sekelenao@protonmail.com>
2024-04-28 20:31:15 +0200

    x

--- src/main/java/fr/sekelenao/skcsv/csv/CsvColumn.java
+++ src/main/java/fr/sekelenao/skcsv/csv/CsvColumn.java
@@ -6,5 +6,5 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-    @Target({ElementType.RECORD_COMPONENT})
-    public @interface CsvColumn {}
+@Target({ElementType.RECORD_COMPONENT})
+public @interface CsvColumn {}
