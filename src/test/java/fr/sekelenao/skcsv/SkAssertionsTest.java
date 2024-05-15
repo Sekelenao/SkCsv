52f7d510304a7f8e5a3e48965f2a69bd4f69cf0c
Sekelenao <Sekelenao@protonmail.com>
2024-05-15 22:43:23 +0200

    removed imports

--- src/test/java/fr/sekelenao/skcsv/SkAssertionsTest.java
+++ src/test/java/fr/sekelenao/skcsv/SkAssertionsTest.java
@@ -1,17 +1,12 @@
 package fr.sekelenao.skcsv;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 final class SkAssertionsTest {
 
