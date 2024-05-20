8bf88b7356cf7079e85afa6dd329a874580abcdf
Sekelenao <Sekelenao@protonmail.com>
2024-05-20 23:39:11 +0200

    Add javadoc

--- src/main/java/fr/sekelenao/skcsv/CsvColumn.java
+++ src/main/java/fr/sekelenao/skcsv/CsvColumn.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *  Indicates that the annotated record component should be considered when exporting records to a CSV file via the
+ *  {@link SkCsvRecords} class.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.RECORD_COMPONENT})
 public @interface CsvColumn {}
